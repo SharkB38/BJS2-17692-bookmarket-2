4db655c9f28a7c1ed3d9c1326ff5197648b1f3c8
SharkB38 <val.wolf.ne@gmail.com>
2024-08-02 22:46:04 +0900

    Revert "Change days to open"
    This reverts commit 99edc909c22a3337debf41064bbf80b8e882eea7.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 1 месяц.");
+        System.out.println("Открытие магазина ожидается через 2 месяца.");
     }
 }
